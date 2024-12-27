cea2c72d70975d98f8ac43c5290991920c0d69ad
Kalyan <kalyankumarpichuka3@gmail.com>
2024-12-27 20:16:32 +0530

    Add file read assignment using interface

---
+++ exercise_75/main.go
@@ -0,0 +1,17 @@
+package main
+
+import (
+	"fmt"
+	"io"
+	"os"
+)
+
+func main() {
+	fmt.Println(os.Args[1])
+	f, err := os.Open(os.Args[1])
+	if err != nil {
+		fmt.Println("Error:", err)
+		panic(err)
+	}
+	io.Copy(os.Stdout, f)
+}
