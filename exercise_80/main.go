ab1c1df72af07abb78a268c5d4ea42c6b851369c
Kalyan <kalyankumarpichuka3@gmail.com>
2024-12-28 23:17:40 +0530

    Add channels realted code

--- exercise_80/main.go
+++ exercise_80/main.go
@@ -1,1 +1,35 @@
 package main
+
+import (
+	"fmt"
+	"net/http"
+	"time"
+)
+
+func checkLink(link string, c chan string) {
+	_, err := http.Get(link)
+	if err != nil {
+		fmt.Println(link, "might be down!")
+		c <- link
+		return
+	}
+	fmt.Println(link, "is up!")
+	c <- link
+}
+
+func main() {
+
+	links := []string{"http://google.com", "http://facebook.com", "http://golang.org", "http://amazon.com"}
+
+	c := make(chan string)
+
+	for _, link := range links {
+		go checkLink(link, c)
+		fmt.Println(<-c)
+	}
+
+	for l := range c {
+		time.Sleep(time.Second)
+		go checkLink(l, c)
+	}
+}
